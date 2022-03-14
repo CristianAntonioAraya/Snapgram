50b8c06a4828691c8b1f4bedf372433edf4c374a
Dev Cristian Araya <dev.arayacristian@gmail.com>
2022-03-14 08:46:03 -0300

    [feat] Add responsive desegin to Search Page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,15 +4,32 @@ import Background from '../componets/Background'
 
 const SearchPage = () => {
 
-    const {searchText} = useParams()
-    console.log(searchText)
+  const {searchText} = useParams()
 
+  const handleOnSubmit = (e) =>{
+    e.preventDefault()
+  }
 
   return (
     <div className='search__container'>
         <Background/>
         <div className='search__box'>
-            pep
+            <form className='search__form' onSubmit={handleOnSubmit}>
+              <h2 className='search__title'>SnapGram</h2>
+              <div className='search__input-container'>
+                <input className='search__input'/>
+                <button className='search__button' type='submit' >submit</button>
+              </div>
+            </form>
+            <div className='search__content'>
+              <div className='search__images'>
+                <p>dsaksaks</p>
+                <p>dsaksaks</p>
+                <p>dsaksaks</p>
+                <p>dsaksaks</p>
+                <p>dsaksaks</p>
+              </div>
+            </div>
         </div>
         <p>{searchText}</p>
     </div>
